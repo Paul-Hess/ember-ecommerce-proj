0e3267431033f3a6d6d2a35f01f3c41e51b41c73
keir staple and paul hess <Guest@epicodus-12.local>
2016-04-07 16:41:23 -0700

    remove faulty beforeModel

--- app/components/perfume-details/component.js
+++ app/components/perfume-details/component.js
@@ -1,6 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  getAverage: Ember.computed.map('perfume.ratings', function(rating, index) {
+    console.log(rating);
+    return rating[index];
+  }),
   actions: {
     addComment(params) {
       this.sendAction('addComment', params);
--- app/login/route.js
+++ app/login/route.js
@@ -1,17 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  beforeModel: function() {
-    return this.get("session").fetch().catch(function() {});
-  },
+  // beforeModel: function() {
+  //   return this.get("session").fetch().catch(function() {});
+  // },
   actions: {
     signIn: function(params) {
+      let _this = this;
       this.get("session").open("firebase", {
         provider: "password",
         email: params.email,
         password: params.password
     }).then(function(data) {
-      this.transitionTo('index');
+      _this.transitionTo('index');
       });
     }
   }
